2e5938ba166d252123c8c933aa4a919715d26ab1
EisnerMarko <mareis01@easv365.dk>
2025-09-09 00:50:57 +0200

    home page draft

--- wp-content/themes/customtheme/header.php
+++ wp-content/themes/customtheme/header.php
@@ -7,7 +7,7 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/iconify/2.2.1/iconify.min.js"></script>
   <title><?php bloginfo("name"); ?></title>
   <meta name="description" content="<?php bloginfo("description"); ?>">
-  <link rel="icon" href="<?php echo get_template_directory_uri() . '/css/img/.png' ?>" type="image/png">
+  <link rel="icon" href="<?php echo get_template_directory_uri() . '/css/img/YumGo.png' ?>" type="image/png">
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?> class="w-full h-full">
@@ -15,8 +15,13 @@
     <header class="w-full h-17 bg-white">
       <nav class="h-full">
         <ul class="text-black flex justify-around items-center h-full">
+                <li class="w-25">
+                  <a href="<?php echo esc_url(home_url('/')); ?>">
+                    <img src="<?php echo get_template_directory_uri() . '/css/img/YumGoWide.png' ?>" alt="Logo">
+                  </a>
+                </li>
                 <li class="text-base font-bold">
-                  <a href="<?php echo esc_url(get_permalink(get_page_by_path(''))); ?>">HOME</a>
+                  <a href="<?php echo esc_url(home_url('/')); ?>">HOME</a>
                 </li>
                 <li class="text-base font-bold">
                   <a href="<?php echo esc_url(get_permalink(get_page_by_path('yumgo-blogs'))); ?>">BLOGS</a>
@@ -28,17 +33,30 @@
                   <a href="<?php echo esc_url(get_permalink(get_page_by_path(''))); ?>">CART</a>
                 </li>
                 <li class="relative" id="lang-dropdown">
-                  <button id="lang-toggle" class="flex items-center px-2 py-1 border rounded bg-white hover:bg-gray-100">
+                  <?php
+                    $languages = pll_the_languages(array('raw' => 1));
+                    $current_flag = '';
+                    foreach ($languages as $lang) {
+                      if ($lang['current_lang']) {
+                        $flag = $lang['flag'];
+                        if (filter_var($flag, FILTER_VALIDATE_URL)) {
+                          $current_flag = '<img src="' . esc_url($flag) . '" alt="' . esc_attr($lang['name']) . '" class="inline w-8 h-6 mr-1" />';
+                        }
+                        break;
+                      }
+                    }
+                  ?>
+                  <button id="lang-toggle" class="flex items-center px-2 py-1 bg-white hover:bg-gray-100">
+                    <?php echo $current_flag; ?>
                     <span class="iconify" data-icon="mdi:chevron-down"></span>
                   </button>
-                  <ul id="lang-menu" class="absolute left-0 mt-2 w-12 bg-white border rounded shadow-lg z-10 hidden">
+                  <ul id="lang-menu" class="absolute left-0 mt-2 w-12 bg-white shadow-lg z-10 hidden">
                     <?php
-                    $languages = pll_the_languages(array('raw' => 1));
                     foreach ($languages as $lang) {
                       if ($lang['current_lang']) continue;
                       $flag = $lang['flag'];
                       if (filter_var($flag, FILTER_VALIDATE_URL)) {
-                        $flag = '<img src="' . esc_url($flag) . '" alt="' . esc_attr($lang['name']) . '" class="inline w-6 h-6" />';
+                        $flag = '<img src="' . esc_url($flag) . '" alt="' . esc_attr($lang['name']) . '" class="inline w-8 h-6" />';
                       }
                       echo '<li>
                         <a href="' . esc_url($lang['url']) . '" class="block px-2 py-1 hover:bg-gray-100">'
--- wp-content/themes/customtheme/index.php
+++ wp-content/themes/customtheme/index.php
@@ -1,9 +1,43 @@
 <?php get_header(); ?>
-<main>
-<!-- main for HTML5 SEO -->
-    
+    <?php if (have_posts()) : ?>
+        <?php while (have_posts()) : the_post();?>
 
+        <main class="min-h-screen w-full">
+            <section class="w-full flex justify-center z-0 overflow-hidden relative">
+                    <?php if ($img = get_field('main_hero_image')): ?>
+                        <img src="<?php echo esc_url($img['url']); ?>" alt="<?php echo esc_attr($img['alt']); ?>" class="w-full object-cover" />
+                    <?php endif; ?>
+                    <!-- FIX :D -->
+                    <div class="flex items-center justify-center absolute top-2/3 left-1/2 md:left-6/11 text-center gap-10 px-4">
+                        <a href="#vendors" class="bg-yellow-300 text-black font-bold px-6 py-2 rounded shadow hover:bg-yellow-400 transition">Meet our vendors</a>
+                        <a href="#about" class="bg-yellow-200 text-black font-bold px-6 py-2 rounded shadow hover:bg-yellow-300 transition">About us</a>
+                    </div>
+                </div>
+            </section>
 
+            <section class="flex flex-col md:flex-row items-center justify-between px-8 py-16 max-w-7xl mx-auto">
+                    
+                <div class="md:w-1/2 w-full flex flex-col items-center md:items-start">
+                    <?php if ($img = get_field('main_headline_image')): ?>
+                        <img src="<?php echo esc_url($img['url']); ?>" alt="<?php echo esc_attr($img['alt']); ?>" class="w-full max-w-md object-contain" />
+                    <?php endif; ?>
+                    <?php if ($text = get_field('first_text_on_fp')): ?>
+                        <p class="p-6 text-black leading-relaxed">
+                            <?php echo esc_html($text); ?>
+                        </p>
+                    <?php endif; ?>
+                </div>
 
-</main>
+                <div class="md:w-1/2 w-full flex justify-center mb-8 md:mb-0">
+                    <?php if ($img = get_field('first_side_image')): ?>
+                        <img src="<?php echo esc_url($img['url']); ?>" alt="<?php echo esc_attr($img['alt']); ?>" class="w-2/3 max-w-xs mb-6 object-contain" />
+                    <?php endif; ?>
+                </div>
+
+            </section>
+
+        </main>
+
+        <?php endwhile; ?>
+    <?php endif; ?>
 <?php get_footer(); ?>
